fea2ad29e21b84bbcfa28deaa9bd52638e8fafa5
blockiosaurus <blockiosaurus@gmail.com>
2023-12-28 17:22:19 -0500

    Adding CLI command to create shards.

--- clients/cli/index.ts
+++ clients/cli/index.ts
@@ -124,6 +124,37 @@ inscribeCmd.command('nft')
         }
     });
 
+const createCmd = program.command('create');
+
+createCmd.command('shards')
+    .description('Inscribe an existing NFT')
+    .option('-r --rpc <string>', 'The endpoint to connect to.')
+    .option('-k --keypair <string>', 'Solana wallet location')
+    .action(async (str: any, options: any) => {
+        const { rpc, keypair } = options.opts();
+
+        let umi = createUmi(rpc);
+        umi = loadWalletKey(umi, keypair);
+        umi.use(MplInscription());
+
+        for (let shardNumber = 0; shardNumber < 32; shardNumber++) {
+            const shardAccount = findInscriptionShardPda(umi, { shardNumber });
+
+            // Check if the account has already been created.
+            let shardData = await safeFetchInscriptionShard(umi, shardAccount);
+
+            if (!shardData) {
+                console.log(`Creating shard ${shardNumber}...`);
+                await createShard(umi, {
+                    shardAccount,
+                    shardNumber,
+                }).sendAndConfirm(umi, { confirm: { commitment: 'confirmed' } });
+            } else {
+                console.log(`Shard ${shardNumber} already exists.`);
+            }
+        }
+    });
+
 program.parse(process.argv);
 
 export function loadWalletKey(umi: Umi, keypair: string): Umi {
