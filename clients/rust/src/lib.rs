f0c557d7ea7c6a468d000e7dbbfc7e5089b6c502
blockiosaurus <blockiosaurus@gmail.com>
2023-12-31 11:49:35 -0500

    Exporting internal functions in rust.

--- clients/rust/src/lib.rs
+++ clients/rust/src/lib.rs
@@ -1,4 +1,10 @@
 mod generated;
+#[path = "./generated/instructions/initialize.rs"]
+mod initialize;
+#[path = "./generated/instructions/initialize_from_mint.rs"]
+mod initialize_from_mint;
 
 pub use generated::programs::MPL_INSCRIPTION_ID as ID;
 pub use generated::*;
+pub use initialize::*;
+pub use initialize_from_mint::*;
